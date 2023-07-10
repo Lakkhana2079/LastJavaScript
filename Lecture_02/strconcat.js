55a4785c29e75350c037d8ef28e575e397381548
lakkhana2079 <lakkhana2079@gmail.com>
2023-07-10 21:50:37 +0700

    update

---
+++ Lecture_02/strconcat.js
@@ -0,0 +1,14 @@
+let str1= "Hello";
+let str2= "World!"; 
+
+//Using the + operator 
+console.log("********Using the + operator********\n"); 
+console.log(strl + str2); 
+console.log(strl + "Big" + str2); 
+
+console.log("\n********Adding Spacing********\n");
+//Adding Spacing 
+str1 = "Hello "; 
+
+console.log(str1 + str2); 
+console.log(strl + "Big " + str2)
